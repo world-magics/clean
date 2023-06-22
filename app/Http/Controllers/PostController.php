a3e7702302c539fc34174576d35d7dc68432d81b
world-magics <sherzodyarashev1997@gmail.com>
2023-06-22 10:26:46 +0500

    add new repo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -128,10 +128,11 @@ class PostController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
+
      */
     public function destroy($id)
     {
         //
     }
-    
+
 }
